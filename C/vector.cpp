63f98a13b5d89aefa2528d95e18d57c0db8efa83
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-06-29 13:12:52 +0200

    Create vector.cpp

---
+++ C/vector.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	vector<int> w;
+	int liczba;
+	for (int i = 0; i < 5; i++)
+	{
+		cin >> liczba;
+		w.push_back(liczba); //dodaje nowy element na koniec vectora
+	}
+	cout << "Ktory element vectora wypisac? ";
+	cin >> liczba;
+	cout << w[liczba]<<endl;
+
+	cout << "Liczba elemntow vectora " << w.size() << endl;
+
+	/*inne metody vectora
+	begin() iterator na pierwszy element
+	end() iterator na element za ostatnim elementem vectora
+	erase(iterator na element do usuniecia, iterator na pierwszy element zakresu, iterator na element za ostatni (np. end() ) ) usuwa element z vectora
+	pop_back() usuwa ostatni element
+	*/
+	system("PAUSE");
+	return 0;
+}
