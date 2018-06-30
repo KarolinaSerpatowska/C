bb790a12af10b419536dda8d7db3fa250a064881
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-06-30 11:31:47 +0200

    Update szablon_funkcji.cpp

--- Cpp/szablon_funkcji.cpp
+++ Cpp/szablon_funkcji.cpp
@@ -11,19 +11,20 @@ type funkcja2(type a, type b)
 
 
 
+
 int main()
 {
 	double a, b;
 	a = 1.5;
 	b = 2.5;
 
-	cout << funkcja2(a, b) << endl;
+	cout << funkcja2<double>(a, b) << endl;
 
 	int x, y;
 	x = 1;
 	y = 2;
 
-	cout << funkcja2(x, y) << endl;
+	cout << funkcja2<int>(x, y) << endl;
 
 	system("PAUSE");
 	return 0;
