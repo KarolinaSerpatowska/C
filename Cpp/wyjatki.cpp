653264458bbf7ed36e76cb21e9895fba2c1c74aa
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-07-04 17:35:35 +0200

    Create wyjatki.cpp

---
+++ Cpp/wyjatki.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+float dzielenie(int a, int b)
+{
+	if (b == 0)
+	{
+		string wyjatek = "nie mozna dzielic przez 0";
+		throw wyjatek;
+	}
+
+	return a / b;
+}
+
+int main()
+{
+	try
+	{
+		dzielenie(2, 0);
+	}
+	catch(string w)
+	{
+		cout << "Wyjatek: " << w << endl;
+	}
+
+	system("PAUSE");
+	return 0;
+}
