de736b27032b77bad3cb8be2f7a7be897cb59f20
KarolinaSerpatowska <32620719+KarolinaSerpatowska@users.noreply.github.com>
2018-07-05 21:01:06 +0200

    Update prepare_to_die.cpp

--- Cpp/prepare_to_die.cpp
+++ Cpp/prepare_to_die.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+template<class type>
+class MagicBox
+{
+	type dupa;
+
+public:
+	void setDupa(type d)
+	{
+		dupa = d;
+	}
+	type getDupa()
+	{
+		return dupa;
+	}
+};
+
+template<>
+class MagicBox<string>
+{
+	string dupa;
+
+public:
+	void setDupa(string d)
+	{
+		dupa = d;
+	}
+	string getDupa()
+	{
+		return dupa+"Prepare to die!";
+	}
+};
+
+template<class type, int size>
+class SuperMagicBox
+{
+	type *tab;
+
+public:
+	SuperMagicBox()
+	{
+		tab = new int[size];
+	}
+	~SuperMagicBox()
+	{
+		delete[] tab;
+	}
+	type &operator[](int index)
+	{
+		return tab[index];
+	}
+	void settab(type a) //funkcja dla testu, wstawia przekazana wartosc na pierwsze miejsce w tablicy
+	{
+		tab[0] = a;
+	}
+};
 
 template<class type>
 type &funkcja(type *begin, type *end)
@@ -23,8 +80,21 @@ int main()
 	tab[1] = 10;
 	tab[2] = 1;
 
+	cout << funkcja<int>(tab, &tab[2]) << endl;
 
-	cout << funkcja<int>(tab, &tab[2])<<endl;
+	MagicBox<int> box;
+	box.setDupa(6);
+	cout << box.getDupa() << endl;
+
+	MagicBox<string> box2;
+	box2.setDupa("Masakra ");
+	cout << box2.getDupa() << endl;
+
+	SuperMagicBox<int, 2> sbox;
+	sbox.settab(2);
+	cout << sbox[0] << endl;
+	
+
 
 	system("PAUSE");
 	return 0;
